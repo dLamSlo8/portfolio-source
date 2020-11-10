053820a012d6f1b54a35aaa3e3d53221045b479b
Derek Lam <dereklam67@gmail.com>
2020-11-10 09:52:03 -0800

    feat: more accessibility

--- src/components/layouts/SectionLayout.jsx
+++ src/components/layouts/SectionLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function SectionLayout({ id, icon, title, bodyClass, children }) {
     return (
-        <section id={id} className="section-layout">
+        <section id={id} className="section-layout" aria-label={title}>
             <header className="section-layout__header">
                 <div className="icon-wrapper icon-wrapper--primary section-layout__header-icon" aria-hidden="true">
                     <div className="icon icon--secondary">{icon}</div>
